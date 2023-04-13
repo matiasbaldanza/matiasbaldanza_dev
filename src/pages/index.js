7172173c6c4d88627feff65a87e93be97c035f3e
Matias Baldanza <matiasbaldanza@gmail.com>
2023-04-13 12:33:02 -0300

    feat: initial homepage structure (blog-first)

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,50 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>Hello, World! 👋</div>
+      <div className='container mx-auto px-6 lg:max-w-5xl flex flex-col min-h-screen'>
+        <header className='flex justify-between py-6 mb-10'>
+          <span>😁 Matías Baldanza</span>
+          <nav>
+            <ul className='flex gap-4'>
+              <li>Blog</li>
+              <li>Portfolio</li>
+            </ul>
+          </nav>
+        </header>
+        <main className='max-h-screen flex-1'>
+          <ul className='divide-y-2'>
+            <li className='flex justify-between p-4'>
+              Título del artículo<span>Fecha</span>
+            </li>
+            <li className='flex justify-between p-4'>
+              Título del artículo<span>Fecha</span>
+            </li>
+            <li className='flex justify-between p-4'>
+              Título del artículo<span>Fecha</span>
+            </li>
+            <li className='flex justify-between p-4'>
+              Título del artículo<span>Fecha</span>
+            </li>
+            <li className='flex justify-between p-4'>
+              Título del artículo<span>Fecha</span>
+            </li>
+            <li className='flex justify-between p-4'>
+              Título del artículo<span>Fecha</span>
+            </li>
+            <li className='flex justify-between p-4'>
+              Título del artículo<span>Fecha</span>
+            </li>
+            <li className='flex justify-between p-4'>
+              Título del artículo<span>Fecha</span>
+            </li>
+          </ul>
+        </main>
+        <footer className='py-6 flex flex-col gap-2'>
+          <p>Call to action1</p>
+          <p>Call to action2</p>
+          <p>Call to action3</p>
+          <p>Contact - Copyright, etc.</p>
+        </footer>
+      </div>
   )
 }
