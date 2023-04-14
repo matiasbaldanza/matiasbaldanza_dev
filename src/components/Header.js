155c8620e6e504ceca43d09f1a92172639799744
Matias Baldanza <matiasbaldanza@gmail.com>
2023-04-14 13:11:13 -0300

    fix: disable unused nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,13 @@ import Link from "next/link"
 export default function Header() {
     return (
         <header className='flex justify-between py-6 mb-10'>
-            <span>✌️😁 Matías Baldanza</span>
+            <Link
+                href='/'
+            ><span>✌️😁 Matías Baldanza</span></Link>
             <nav>
                 <ul className='flex gap-4'>
-                <li>Blog</li>
-                <li>Portfolio</li>
+                <li className="text-gray-500 cursor-not-allowed">Blog</li>
+                <li className="text-gray-500 cursor-not-allowed">Portfolio</li>
                 </ul>
             </nav>
         </header>
