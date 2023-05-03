a88494a2493a4e6b596af9c26c0fffc8f9a3fd58
Matias Baldanza <matiasbaldanza@gmail.com>
2023-05-03 12:53:59 -0300

    previo a instalar shadcn/ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,51 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography'),
   ],
+/*   theme: {
+    colors: {
+      primary: "#570df8",
+      "primary-content": "#ffffff",
+      secondary: "#f000b8",
+      "secondary-content": "#ffffff",
+      accent: "#37cdbe",
+      "accent-content": "#163835",
+      neutral: "#3d4451",
+      "neutral-content": "#ffffff",
+      "base-100": "#ffffff",
+      "base-200": "#F2F2F2",
+      "base-300": "#E5E6E6",
+      "base-content": "#1f2937",  
+    },
+  }, */
 }
+
+/* {
+  light: {
+    primary: "#570df8",
+    "primary-content": "#ffffff",
+    secondary: "#f000b8",
+    "secondary-content": "#ffffff",
+    accent: "#37cdbe",
+    "accent-content": "#163835",
+    neutral: "#3d4451",
+    "neutral-content": "#ffffff",
+    "base-100": "#ffffff",
+    "base-200": "#F2F2F2",
+    "base-300": "#E5E6E6",
+    "base-content": "#1f2937",
+  },
+  dark: {
+    primary: "#ffffff",
+    secondary: "#152747",
+    accent: "#513448",
+    neutral: "#171618",
+    "neutral-content": "#dca54c",
+    "base-100": "#09090b",
+    "base-200": "#171618",
+    "base-300": "#2e2d2f",
+    "base-content": "#dca54c",
+    info: "#66c6ff",
+    success: "#87d039",
+    warning: "#e2d562",
+    error: "#ff6f6f",
+  } */
