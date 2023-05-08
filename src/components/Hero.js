b75dd79086cbd63023339c0bf45d154aa038e089
Matias Baldanza <matiasbaldanza@gmail.com>
2023-05-08 13:56:40 -0300

    content: pages/building-in-public + changelog

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,10 +7,11 @@ function Hero () {
         <div className='max-w-md'>
           <h1 className='text-5xl font-bold'>Hello, there!</h1>
           <p className='py-6'>
-            This is a simple blog built with Next.js and Tailwind CSS.
+            This is a simple blog, custom-made <br />
+            with Next.js 13 and Tailwind CSS.
           </p>
           <p>
-            It's a <em>work in progress</em>, and I'm building it in public, mostly on Twitch. You can check out the source code on my <Link href='https://github.com/matiasbaldanza'>GitHub</Link>.
+            It's a <em>work in progress</em>, (here's the <Link href='/building-in-public#changelog'>changelog</Link>) and I'm building it in public, mostly on Twitch. You can check out the source code on my <Link href='https://github.com/matiasbaldanza'>GitHub</Link>.
           </p>
         </div>
       </div>
---
+++ src/pages/building-in-public.js
@@ -0,0 +1,58 @@
+// Components
+import Layout from '@/components/Layout'
+import DateLabel from '@/components/DateLabel'
+import Balancer from 'react-wrap-balancer'
+
+export default function Home ({ articlesData }) {
+  return (
+    <Layout>
+      <header>
+        <h1 className='mb-6 text-4xl font-bold text-center'>
+          Building in Public
+        </h1>
+      </header>
+      <section className='mb-10 [&>*]:mb-4' id='why'>
+        <p>For as long as I can remember, I've been the type of person that comes up with at least a couple of project ideas <em>a day</em>, maybe starts a few projects a year, and usually finishes none.</p>
+        <p><strong>Building in public</strong> helped me break out of that loop.</p>
+        <p>Over the past few years, the indie hacker community <em>(with <strong>indie hacker</strong> being just a fancy name for <strong> boostrapped entrepreneur</strong>)</em> has embraced the <strong>building in public</strong> label, with an obvious benefit: </p>
+        <p>They get to receive early feedback from (hopefully) real users, and grow a community while they build their products.</p>
+        <p>I'm not an indie hacker, but I started learning and building projects in public in September 2022 and 8 months into it (this is May 8, 2023), I've made more progress and built more projects than ever before.</p>
+        <p>Building in public has also brought in an unexpected benefits: A community of like-minded people that bring insights, feedback, and encouragement.</p>
+        <p>From time to time, I'll be sharing some of those insights and stories here and in my blog.</p>
+        <p>Below, you will find a Changelog of this project.</p>
+
+      </section>
+      <section className='mb-10' id='changelog'>
+        <h2 className='mb-6 text-3xl font-bold tracking-tight'>
+          Changelog
+        </h2>
+        <ul className='flex flex-col divide-y-[1px] border-y-[1px] selection:divide-gray-200'>
+          <li className='flex items-center justify-between gap-10 p-4 text-sm hover:bg-gray-100 sm:text-lg'>
+            <div className='flex flex-col gap-1'>
+              <p>Soft launch</p>
+              <p className='text-xs'>Dark mode. English. Footer. Basic styling.</p>
+            </div>
+            <DateLabel
+              className='text-sm text-right text-gray-500 min-w-max'
+              options={{ month: 'short', day: 'numeric' }}
+            >
+              2023-05-08
+            </DateLabel>
+          </li>
+          <li className='flex items-center justify-between gap-10 p-4 text-sm hover:bg-gray-100 sm:text-lg'>
+            <div className='flex flex-col gap-1'>
+              <p>Pre-launch</p>
+              <p className='text-xs'>Next.js blog using the pages router and getStaticProps(). TailwindCSS. Dark mode. Custom components.</p>
+            </div>
+            <DateLabel
+              className='text-sm text-right text-gray-500 min-w-max'
+              options={{ month: 'short', day: 'numeric' }}
+            >
+              2023-05-05
+            </DateLabel>
+          </li>
+        </ul>
+      </section>
+    </Layout>
+  )
+}
